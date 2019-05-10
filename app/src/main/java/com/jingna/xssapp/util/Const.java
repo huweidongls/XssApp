903147123e77599c0a852d6ba1e70236aaaa7616
huweidong <466463054@qq.com>
2019-05-10 10:14:16 +0800

    3213

---
+++ app/src/main/java/com/jingna/xssapp/util/Const.java
@@ -0,0 +1,13 @@
+package com.jingna.xssapp.util;
+
+/**
+ * Created by Administrator on 2019/2/21.
+ */
+
+public class Const {
+
+    public static final String BASE_URL = "http://192.168.2.37:8888/";
+//    public static final String BASE_URL = "http://192.168.2.211:8888/";
+//    public static final String BASE_URL = "http://jshop.5ijiaoyu.cn/";
+
+}
---
+++ app/src/main/java/com/jingna/xssapp/util/SpUtils.java
@@ -0,0 +1,64 @@
+package com.jingna.xssapp.util;
+
+import android.content.Context;
+
+import com.vise.xsnow.cache.SpCache;
+
+/**
+ * Created by Administrator on 2018/10/26.
+ */
+
+public class SpUtils {
+
+    private static SpCache spCache;
+    public static String USER_ID = "user_id";
+    public static String PHONENUM = "phonenum";
+    public static String TOKEN = "token";
+    public static String SEARCH_HISTORY = "search_history";
+
+    public static void setSearchHistory(Context context, String token){
+        spCache = new SpCache(context, "user_info");
+        spCache.put(SEARCH_HISTORY, token);
+    }
+
+    public static String getSearchHistory(Context context){
+        spCache = new SpCache(context, "user_info");
+        return spCache.get(SEARCH_HISTORY, "");
+    }
+
+    public static void setToken(Context context, String token){
+        spCache = new SpCache(context, "user_info");
+        spCache.put(TOKEN, token);
+    }
+
+    public static String getToken(Context context){
+        spCache = new SpCache(context, "user_info");
+        return spCache.get(TOKEN, "0");
+    }
+
+    public static void setPhoneNum(Context context, String phonenum){
+        spCache = new SpCache(context, "user_info");
+        spCache.put(PHONENUM, phonenum);
+    }
+
+    public static String getPhoneNum(Context context){
+        spCache = new SpCache(context, "user_info");
+        return spCache.get(PHONENUM, "0");
+    }
+
+    public static void setUserId(Context context, String userid){
+        spCache = new SpCache(context, "user_info");
+        spCache.put(USER_ID, userid);
+    }
+
+    public static String getUserId(Context context){
+        spCache = new SpCache(context, "user_info");
+        return spCache.get(USER_ID, "0");
+    }
+
+    public static void clear(Context context){
+        spCache = new SpCache(context, "user_info");
+        spCache.clear();
+    }
+
+}
